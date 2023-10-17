02c3ae9ed2863906ddee2d614284ddb617840834
VitorHugoAntunes <vitor4ntunes@gmail.com>
2023-10-17 16:01:35 -0300

    Added radix-ui & phosphor-icons, created new React components and stories components

---
+++ packages/react/src/components/MultiStep/index.tsx
@@ -0,0 +1,25 @@
+import { Label, MultiStepContainer, Step, Steps } from './styles'
+
+export interface MultiStepProps {
+  size: number
+  currentStep?: number
+}
+
+export function MultiStep({ size, currentStep = 1 }: MultiStepProps) {
+  return (
+    <MultiStepContainer>
+      <Label>
+        Passo {currentStep} de {size}
+      </Label>
+
+      <Steps css={{ '--steps-size': size }}>
+        {/* Criando um novo array a partir da variavel size com seu tamanho, */}
+        {/* retornando um Step para cada posicao do array */}
+        {/* e adicionando a propriedade active no currentStep */}
+        {Array.from({ length: size }, (_, i) => i + 1).map((step) => {
+          return <Step key={step} active={currentStep >= step} />
+        })}
+      </Steps>
+    </MultiStepContainer>
+  )
+}
