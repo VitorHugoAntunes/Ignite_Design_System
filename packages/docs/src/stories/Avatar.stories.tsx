a7969b182fd5c79bda743ac1ad235526e19b46d8
VitorHugoAntunes <vitor4ntunes@gmail.com>
2023-10-17 16:40:47 -0300

    Components display name, storybook actions log, components controll and components accessibility

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/vitorhugoantunes.png',
     alt: 'Vitor Hugo Antunes',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
